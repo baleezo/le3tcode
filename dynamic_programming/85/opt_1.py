4e1a63db9c51692c812bd40cf56281f554bcb9a3
baleezo <baleezo@teamt5.org>
2022-02-10 22:27:20 +0800

    add 85 opt_3 amd mod opt_1

--- dynamic_programming/85/opt_1.py
+++ dynamic_programming/85/opt_1.py
@@ -1,4 +1,5 @@
 class Solution(object):
+
     def maximalRectangle(self, matrix):
         """
         :type matrix: List[List[str]]
@@ -6,8 +7,8 @@ class Solution(object):
         """
 
         m, n = len(matrix), len(matrix[0])
-        ans = 0
         dp = [[0] * n for _ in range(m)]
+        ans = 0
 
         for i in range(m):
             for j in range(n):
@@ -15,18 +16,19 @@ class Solution(object):
                     if j - 1 < 0:
                         dp[i][j] = 1
                     else:
-                        dp[i][j] = dp[i][j - 1] + 1
-
-                if matrix[i][j] != '1':
+                        dp[i][j] = dp[i][j - 1 ] + 1
+                else:
                     continue
 
-                current_w = n
+                curr_w = n
+
                 for h in range(i + 1):
-                    current_w = min(current_w, dp[i - h][j])
-                    if current_w == 0:
+                    curr_w = min(curr_w, dp[i - h][j])
+
+                    if curr_w == 0:
                         break
 
-                    ans = max(ans, (h + 1) * current_w)
+                    ans = max(ans, (h + 1) * curr_w)
 
         return ans
 
