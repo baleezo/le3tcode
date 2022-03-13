1b696ee1301b6f8e4efed3f40ad56fd59c6df486
baleezo <baleezo@teamt5.org>
2022-03-13 17:35:07 +0800

    add 209

---
+++ sliding_window/209/bf.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        """
+        :type target: int
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        p_sum = [0] * len(nums)
+        p_sum[0] = nums[0]
+        for i in range(1, len(nums)):
+            p_sum[i] = nums[i] + p_sum[i - 1]
+
+        ans = len(nums) + 1
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                if p_sum[j] - p_sum[i] + nums[i] >= target:
+                    ans = min(ans, j - i + 1)
+                    break
+
+        if ans <= len(nums):
+            return ans
+
+        return 0
+
---
+++ sliding_window/209/two_pointers.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        """
+        :type target: int
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        c_sum = 0
+        left = 0
+        ans = len(nums) + 1
+
+        for i in range(len(nums)):
+            c_sum += nums[i]
+
+            while c_sum >= target:
+                ans = min(ans, i - left + 1)
+                c_sum -= nums[left]
+                left += 1
+
+        if ans <= len(nums):
+            return ans
+
+        return 0
