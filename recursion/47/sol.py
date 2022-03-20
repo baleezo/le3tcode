a677b35fb6af8c73c57eb833214577acca1ae55e
baleezo <baleezo@teamt5.org>
2022-03-20 21:33:33 +0800

    enhance permutation ii

--- recursion/47/sol.py
+++ recursion/47/sol.py
@@ -4,22 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        if len(nums) == 1:
-            yield nums
+        def per(nums, s):
+            if s == len(nums) - 1:
+                yield [nums[s]]
 
-        nums= sorted(nums)
-        prev = None
+            curr_h = set()
 
-        for _ in range(len(nums)):
-            p = nums.pop(0)
+            for i in range(s, len(nums)):
+                if nums[i] in curr_h:
+                    continue
+                else:
+                    curr_h.add(nums[i])
 
-            if p == prev:
-                nums.append(p)
-                continue
-            else:
-                prev = p
+                nums[s], nums[i] = nums[i], nums[s]
 
-            for per in self.permuteUnique(nums):
-                yield [p] + per
+                for sub_p in per(nums, s + 1):
+                    yield [nums[s]] + sub_p
 
-            nums.append(p)
+                nums[s], nums[i] = nums[i], nums[s]
+
+        return per(nums, 0) if nums else nums
